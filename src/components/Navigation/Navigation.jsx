66e2c080247acf2365bc7a767d18bae1464467a1
MikazukiMizuki <winstonak13112001@gmail.com>
2022-05-05 21:10:56 +0700

    Menyelesaikan navbar responsive

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,38 +1,41 @@
 import React from 'react'
-import { Navbar, Nav, Button, Container, Form, FormControl, NavLink } from 'react-bootstrap';
+import { Nav, Button, Container, Form, FormControl } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Navigation.css'
 
 const Navigation = () => {
 
   return (
-    <Navbar bg="dark" variant="dark" className="navbar navbar-expand-lg">
-      <Container fluid>
-        <div class="d-flex">
-          <Navbar.Brand>Hotelify</Navbar.Brand>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav">
-              <li><Nav.Item><Nav.Link href="/Home">Home</Nav.Link></Nav.Item></li>
-              <li><Nav.Item><Nav.Link href="/Hotel">Hotel</Nav.Link></Nav.Item></li>
-              <li><Nav.Item><Nav.Link href="/">Booking History</Nav.Link></Nav.Item></li>
-              <li><Nav.Item><Nav.Link href="/About">AboutUs</Nav.Link></Nav.Item></li>
-            </ul>
-          </div>
-        </div>
+    <nav className="navbar navbar-expand-md  bg-dark navbar-dark">
+      {/* <Container fluid> */}
+      <div className="container-fluid justify-content-between">
+        <ul className='nav navbar-nav'>
+          <li><Nav.Item><Nav.Link href="/">Hotelify</Nav.Link></Nav.Item></li>
+          <li><Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item></li>
+          <li><Nav.Item><Nav.Link href="/Hotel">Hotel</Nav.Link></Nav.Item></li>
+          <li><Nav.Item><Nav.Link href="/">Booking History</Nav.Link></Nav.Item></li>
+          <li><Nav.Item><Nav.Link href="/About">About Us</Nav.Link></Nav.Item></li>
 
-        <div class="d-md-flex">
-          <ul className='nav navbar-nav ms-auto'>
-            <li>
-              <Link to="/Login"><Button variant="outline-primary" className="m-2" >Login</Button></Link>
-            </li>
-            <li>
-              <Link to="/Register"><Button variant="outline-danger" className="m-2">Register</Button></Link>
-            </li>
-          </ul>
-        </div>
+          <li className="my-2 d-block d-sm-block d-xl-none d-lg-none d-md-none">
+            <Link to="/Login"><Button variant="outline-primary">Login</Button></Link>
+          </li>
+          <li className="my-2 d-block d-sm-block d-xl-none d-lg-none d-md-none">
+            <Link to="/Register"><Button variant="outline-danger">Register</Button></Link>
+          </li>
+        </ul>
 
+        <ul className='nav navbar-nav'>
+          <li className="mx-2 d-none d-md-block">
+            <Link to="/Login"><Button variant="outline-primary" >Login</Button></Link>
+          </li>
+          <li className="mx-2 d-none d-md-block">
+            <Link to="/Register"><Button variant="outline-danger" >Register</Button></Link>
+          </li>
+        </ul>
+      </div>
 
-        {/* <Form className="d-flex">
+      {/* 
+        <Form className="d-flex">
           <FormControl
             type="search"
             placeholder="Search"
@@ -41,11 +44,10 @@ const Navigation = () => {
           />
           <Button variant="outline-primary">Search</Button>
         </Form> */}
-      </Container>
-    </Navbar>
+      {/* </Container> */}
+    </nav>
   )
 }
-
 export default Navigation
 
 
