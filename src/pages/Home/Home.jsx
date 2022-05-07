893660dbf00558fe6bc165e2705b019ade134486
MikazukiMizuki <winstonak13112001@gmail.com>
2022-05-07 18:13:39 +0700

    Menambahkan data dummy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
+import { useEffect, useState } from "react";
 import Navigation from '../../components/Navigation/Navigation'
 import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
+
+    const [hotels, setHotels] = useState([]);
+
+    useEffect(() => {
+        // memanggil API untuk mengambil data
+        fetch("http://localhost:8000/hotel")
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                // ketika Rest API sukses, simpan data dari response ke dalam state lokal
+                setHotels(data);
+            })
+            .catch((err) => {
+                if (err.name === "AbortError") {
+                    console.log("fetch aborted.");
+                }
+            });
+    }, []);
+
     return (
         <>
             <Navigation />
-            <div className="mb-4">Isi</div>
+            <ul id="hotel-list">
+                {hotels.map((hotel) => (
+                    <div>
+                        <p>{hotel.id}  {hotel.desc}</p>
+                        <p>{hotel.name}</p>
+                    </div>
+                ))}
+            </ul>
             <Footer />
         </>
     )
